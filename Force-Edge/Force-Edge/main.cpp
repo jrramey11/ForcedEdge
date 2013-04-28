aa267c20037acd7b538bd0a1b56935489b39fb99
Jerry Ramet <jerry.ramey@zenbanana.com>
2013-04-28 12:23:19 -0600

    Adding Initial Project

---
+++ Force-Edge/Force-Edge/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Force-Edge
+//
+//  Created by Jerry  Ramey on 4/28/13.
+//  Copyright (c) 2013 Jerry  Ramey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
